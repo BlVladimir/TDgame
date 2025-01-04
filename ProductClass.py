5c9cd4bd6c03b009b36c61a7037b4042bfa1ed49
BlVladimir <vblinov555@gmail.com>
2025-01-04 21:52:35 +0300

    append product class

--- ProductClass.py
+++ ProductClass.py
@@ -1,12 +1,22 @@
 import TowerClass
+import ButtonClass
+
+def buy_tower(additional_image, image, scale,tower_array, damage, radius, coordinate, index, improve_cost_array):
+    if additional_image is not None:
+        tower_array.append(TowerClass.Tower(image, scale, damage, coordinate, index, improve_cost_array, radius, additional_image))
+    else:
+        tower_array.append(TowerClass.Tower(image, scale, damage, coordinate, index, improve_cost_array, radius))
+    return tower_array
 
 class Product:
-    def __init__(self, image, cost, scale, additional_image = None):
+    def __init__(self, image, cost, scale, coordinate, additional_image = None):
         self.__image = image
-        self.__scale = scale
         self.cost = cost
         if additional_image is not None:
             self.__additional_image = additional_image
+            self.button_product = ButtonClass.Button(coordinate[0], coordinate[1], image, scale, scale, buy_tower, additional_image)
 
-    def buy_tower(self, tower_array):
-        tower_array.append(TowerClass.Tower(self.__image, self.__scale))
+        else:
+            self.__additional_image = None
+            self.button_product = ButtonClass.Button(coordinate[0], coordinate[1], image, scale, scale, buy_tower)
+
