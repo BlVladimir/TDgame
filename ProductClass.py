ac3eeb23275d2bf2a3c08779c9fe759aa10e860e
BlVladimir <vblinov555@gmail.com>
2025-01-05 15:41:36 +0300

    append product class

--- ProductClass.py
+++ ProductClass.py
@@ -10,9 +10,10 @@ def buy_tower(additional_image, image, scale,tower_array, damage, radius, coordi
     return tower_array
 
 class Product:
-    def __init__(self, image, cost, scale, coordinate, additional_image = None):
+    def __init__(self, image, cost, scale, coordinate, parameter_tower, additional_image = None):
         self.__image = image
         self.cost = cost
+        self.parameter_tower = parameter_tower
         if additional_image is not None:
             self.__additional_image = additional_image
             self.button_product = ButtonClass.Button(coordinate[0], coordinate[1], image, scale, scale, buy_tower, additional_image)
@@ -23,3 +24,9 @@ class Product:
 
     def draw(self, screen):
         self.button_product.draw(screen)
+
+    def buy(self, event, money, type_tile, is_free):
+        if self.button_product.is_pressed(event) and (money >= self.cost or is_free):
+            match type_tile:
+                case 2:
+                    self.button_product.handle_event_parameter()
