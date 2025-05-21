0e9f42ff80f55d3d64c6cd363701b76e73f5da18
BlVladimir <vblinov555@gmail.com>
2025-05-21 10:22:53 +0300

    tower config

---
+++ scripts/tower/tower_config.py
@@ -0,0 +1,21 @@
+import json
+class TowerConfig:
+    def __init__(self):
+        self.__products = {
+            'common': dict(image="images/tower/common_foundation.png", damage=2, cost=3, radius=1,
+                           improve_cost_array=(4, 6), armor_piercing=False, poison=0, additional_money=2,
+                           additional_image="images/tower/common_gun.png"),
+            'sniper': dict(image="images/tower/common_foundation.png", damage=4, cost=5, radius=2,
+                           improve_cost_array=(6, 8), armor_piercing=False, poison=0, additional_money=0,
+                           additional_image="images/tower/sniper_gun.png"),
+            'anty_shield': dict(image="images/tower/anty_shield.png", damage=3, cost=4, radius=1.5,
+                                improve_cost_array=(5, 7), armor_piercing=True, poison=0, additional_money=0),
+            'venom': dict(image="images/tower/venom_foundation.png", damage=2, cost=5, radius=1,
+                          improve_cost_array=(4, 6), armor_piercing=False, poison=2, additional_money=0,
+                          additional_image="images/tower/venom_gun.png")}
+
+    def get_improve_cost_array(self, type):
+        return self.__products[type]['improve_cost_array']
+
+    def get_image_tuple(self, type):
+        if self.__products
